57919fe9bfa1808ec4a5759f8061fc5c201af6de
David <Rigby335@hotmail.com>
2022-05-20 15:27:27 -0700

    added form eventlistener

---
+++ create-page/create-page.js
@@ -0,0 +1,8 @@
+const createForm = document.getElementById('create-form');
+
+createForm.addEventListener('submit' async (e) => {
+    e.preventDefault();
+    const data = new FormData(createForm);
+
+    
+});
